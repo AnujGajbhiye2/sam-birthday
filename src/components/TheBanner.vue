f0632e0fa889c6b010255616308cd948e0c15c7e
Anuj Gajbhiye <anujgajbhiye97@gmail.com>
2022-05-13 09:46:14 +0530

    added message

--- src/components/TheBanner.vue
+++ src/components/TheBanner.vue
@@ -2,12 +2,22 @@
     <span>
         <div class="row" style="position:relative">
             <div class="col-12">
-                <h3>this is a test message</h3>
+                <h3>It's your birthdayyyyy...🎉🎉🎉</h3>
             </div>
             <div class="col-12">
-                <img src="../assets/sam2.jpg" alt="">
-                <img src="../assets/msg.png" alt="">
-
+                <img class="sam" src="../assets/sam2.jpg" alt="">
+                <img class="msg" src="../assets/msg.png" alt="">
+                <span class="letter">
+                    <h4>Dearest Samiksha,</h4>
+                    <h4>Many many many many Happy Returns of the day. 
+                        <br>I could have never imagined,
+                         the stupid girl I saw 6 years ago <br> playing a racing game on her nokia would grow out to be such a beautiful women. </h4>
+                    <h4>You deserve all the happiness in the world!!</h4>
+                    <h4>Thank you for existing and making all the people around you happy and positive.</h4> <br>
+                    <h4>Yours Truly,<br>
+                        Anuj
+                    </h4>
+                </span>
             </div>
 
         </div>
@@ -24,13 +34,30 @@
     h3{
         top: -250px;
         /* left: 40%; */
+        margin-left: 105px ;
         position: relative;        
     }
-    img{ 
+    img.sam { 
         position: relative;
         width: 300px;
-        top: -130px;
+        top: -293px;
         left: 139px;
+    }
+
+    img.msg {
+        position: relative;
+        margin-left: 250px;
+        width: 900px;
+        top: -300px;
+    }
+    h4 {
+        font-family: 'Oleo Script Swash Caps', cursive;
+    }
+
+    span.letter {
+        position: relative;
+        top: -850px;
+        left: 685px;
     }
 
 </style>
